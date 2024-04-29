52c9f797244f6ebc8df4eb560a4cc3b4b719798b
pedrogamedev <pedrobrunigavioli@gmail.com>
2024-04-29 16:42:23 -0300

    Desafio Concluido!

---
+++ src/main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception);
+        }
+
+    }
+
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm < parametroDois) {
+            int contagem = parametroDois - parametroUm;
+
+            for (int i = 1; i < contagem+1; i++) {
+                System.out.println("Imprimindo o numero " + i);
+            }
+        } else {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+}
